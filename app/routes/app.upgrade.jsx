8a856e1e231335cc55c2178353d19bc32a5c4414
Avinash kumar keshri <kravinash321@gmail.com>
2024-09-14 12:29:11 +0530

    subscription build commit

---
+++ app/routes/app.upgrade.jsx
@@ -0,0 +1,22 @@
+import {  redirect } from "@remix-run/node";
+import { authenticate, MONTHLY_PLAN } from "../shopify.server";
+
+
+export const loader = async ({ request }) => {
+  const { billing, session } = await authenticate.admin(request);
+  let { shop } = session;
+  let myShop = shop.replace(".myshopify.com", "");
+
+  await billing.require({
+    plans: [MONTHLY_PLAN],
+    onFailure: async () => billing.request({
+      plan: MONTHLY_PLAN,
+      isTest: true,
+      //returnUrl: `https://admin.shopify.com/store/subscription-build/apps/subscription-build/app/pricing`,
+      returnUrl:`https://admin.shopify.com/store/${myShop}/apps/subscription-build/app/pricing`
+    }),
+  });
+  // App logic
+
+  return null;
+};
